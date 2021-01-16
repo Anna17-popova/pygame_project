81374f757f2a10467e4ea67338a49eddbdfba9cd
Anna17-popova <74873856+Anna17-popova@users.noreply.github.com>
2021-01-16 17:49:24 +0300

    Add files via upload

--- project.py
+++ project.py
@@ -3,15 +3,31 @@ import sys
 import os
 import random
 
-
 pygame.init()
-size = WIDTH, HEIGHT = 600, 600
+pygame.mixer.init()
+pygame.mixer.music.load('music.mp3')
+sound = pygame.mixer.Sound('game_over.wav')
+size = WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption('Змейка')
+pygame.display.set_caption('Snake')
 clock = pygame.time.Clock()
-SPEED = 16
 SCORE = 0
-FPS = 10
+FPS = 5
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join("data", name)
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as e:
+        print("Cannot load image:", name)
+        raise SystemExit(e)
+    image = image.convert_alpha()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    return image
 
 
 def terminate():
@@ -19,9 +35,102 @@ def terminate():
     sys.exit()
 
 
+all_sprites = pygame.sprite.Group()
+image = load_image('arrow.png')
+cursor = pygame.sprite.Sprite(all_sprites)
+cursor.image = image
+cursor.rect = cursor.image.get_rect()
+pygame.mouse.set_visible(False)
+
+
+def start_screen():
+    intro_text = ["Snake", "",
+                  "made by Anna,",
+                  "with Pasha's help"]
+    button = 'START'
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 70)
+    font1 = pygame.font.Font(None, 20)
+    rendered = font1.render(button, True, (255, 0, 0))
+    rect = rendered.get_rect()
+    rect.y = 40
+    rect.x = 300
+    screen.blit(rendered, rect)
+    text_coord = 85
+    for line in intro_text:
+        string_rendered = font.render(line, True, (154, 48, 1))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.MOUSEMOTION:
+                cursor.rect.topleft = event.pos
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if (rect.x <= event.pos[0] <= rect.x + 150) and (rect.y <= event.pos[1] <= rect.y + 50):
+                    return
+
+        intro_text = ["Snake", "",
+                      "made by Anna,",
+                      "with Pasha's help"]
+        fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+        screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 70)
+        text_coord = 85
+        for line in intro_text:
+            string_rendered = font.render(line, True, (154, 48, 1))
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            screen.blit(string_rendered, intro_rect)
+        font1 = pygame.font.Font(None, 50)
+        rendered = font1.render(button, True, (154, 48, 1))
+        rect = rendered.get_rect()
+        rect.y = 150
+        rect.x = 190
+        pygame.draw.rect(screen, (154, 48, 1), (rect.x - 5, rect.y - 5, 120, 50), width=2)
+        screen.blit(rendered, rect)
+        if pygame.mouse.get_focused():
+            all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+def gameover(f):
+    pygame.mixer.music.stop()
+    intro_text = ["GAME OVER!",
+                  f"Score: {SCORE}"]
+    fon = pygame.transform.scale(load_image('screen.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 50)
+    text_coord = 180
+    for line in intro_text:
+        string_rendered = font.render(line, True, (255, 0, 0))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 150
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    pygame.display.flip()
+    sound.play()
+    pygame.time.delay(3500)
+    pygame.quit()
+    sys.exit()
+
+
 class Ball:
     def __init__(self):
-        self.radius = 7
+        self.radius = 5
         self.x = random.randrange(20, WIDTH - 20)
         self.y = random.randrange(20, HEIGHT - 20)
 
@@ -31,55 +140,70 @@ class Ball:
 
     def draw(self):
         pygame.draw.circle(screen, pygame.Color("red"),
-                           (self.x + self.radius, self.y + self.radius), self.radius)
+                           (self.x, self.y), self.radius)
 
 
 class Snake:
     def __init__(self):
         x, y = random.randrange(100, WIDTH - 100), random.randrange(100, HEIGHT - 100)
         self.position = [x, y]
-        self.width, self.height = 14, 14
+        self.width, self.height = 10, 10
         self.body = [(x, y), (x + self.width, y),
                      (x + 2 * self.width, y)]
         self.direction = 'left'
         self.moving = True
+        self.changed = False
+        self.speed = 12
+        self.flag = False
 
     def move(self):
         if self.moving:
             if self.direction == 'left':
-                self.position[0] -= SPEED
+                self.position[0] -= self.speed
             elif self.direction == 'right':
-                self.position[0] += SPEED
+                self.position[0] += self.speed
             elif self.direction == 'down':
-                self.position[1] += SPEED
+                self.position[1] += self.speed
             elif self.direction == 'up':
-                self.position[1] -= SPEED
+                self.position[1] -= self.speed
 
     def draw(self):
         if self.moving:
             self.body.insert(0, (self.position[0], self.position[1]))
-            if ball.x - 2 <= self.position[0] <= ball.x + ball.radius * 2 + 2\
-                    and ball.y - 2 <= self.position[1] <= ball.y + ball.radius * 2 + 2:
+            if (ball.x - ball.radius <= self.position[0] <= ball.x + ball.radius or ball.x - ball.radius <=
+                self.position[0] + self.width <= ball.x + ball.radius) \
+                    and (ball.y - 5 <= self.position[1] <= ball.y + 5
+                         or ball.y - 5 <= self.position[1] + self.height <= ball.y + 5):
                 ball.change_position()
                 global SCORE
                 SCORE += 1
+                self.changed = True
             else:
                 self.body.pop()
+                self.changed = False
         for cube in self.body:
             pygame.draw.rect(screen, 'blue', (cube[0], cube[1], self.width, self.height))
 
     def crashed(self):
-        if self.position[0] <= 2 or self.position[0] >= WIDTH - 6 \
-                or self.position[1] <= 4 or self.position[1] >= HEIGHT - 6:
-            terminate()
         for cube in self.body[1:]:
-            if self.position[0] == cube[0] and self.position[1] == cube[1]:
-                terminate()
+            if (cube[0] == self.position[0]) and (cube[1] == self.position[1]):
+                self.flag = True
+                break
+        if (self.position[0] <= 2 or self.position[0] > WIDTH - 5
+            or self.position[1] <= 5 or self.position[1] >= HEIGHT - 5) or self.flag:
+            gameover(False)
+
+    def acceleration(self):
+        if self.changed and SCORE % 3 == 0:
+            global FPS
+            FPS += 3
 
 
+start_screen()
 running = True
 snake = Snake()
 ball = Ball()
+pygame.mixer.music.play(-1)
 while running:
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
@@ -108,6 +232,7 @@ while running:
     snake.move()
     snake.draw()
     snake.crashed()
+    snake.acceleration()
     pygame.display.flip()
     clock.tick(FPS)
 pygame.quit()
